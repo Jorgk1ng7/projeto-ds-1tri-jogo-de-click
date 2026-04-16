f513351d4c49c4cb9d8397755fafe9b7600ae13c
darinhaa <hiandara.wosnhaki@escola.pr.gov.br>
2026-04-16 10:32:30 -0300

    up

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-
-export default function Home() {
-  return (
-    <div className="min-h-screen bg-gray-50">
-      {/* Aqui começa o conteúdo da sua página inicial */}
-
-      <h1 className="text-4xl font-bold text-center py-10">
-        Bem-vindo ao meu site
-      </h1>
-
-      {/* O resto do seu conteúdo vai aqui */}
-
-    </div>
-  );
-}
-=======
 import { HelloWave } from '@/components/hello-wave';
 import ParallaxScrollView from '@/components/parallax-scroll-view';
 import { ThemedText } from '@/components/themed-text';
